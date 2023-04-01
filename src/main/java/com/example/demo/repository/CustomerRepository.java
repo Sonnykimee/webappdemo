16221e1270963d687735330defb9e851fad0a1d1
Kim, Sonny <sonnkim@iu.edu>
2023-03-31 20:47:40 -0400

    .

--- src/main/java/com/example/demo/repository/CustomerRepository.java
+++ src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -1,48 +1,11 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Customer;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Repository
-public class CustomerRepository {
-
-    List<Customer> customers = new ArrayList<>();
-
-    public List<Customer> findAll(){
-        return customers;
-    }
-
-    public int create(Customer customer){
-        int id = customers.size() + 1;
-        customer.setId(id);
-        customers.add(customer);
-        return id;
-    }
-
-    public void update(Customer customer, int id){
-        Customer c = getById(id);
-        if(c != null){
-            c.setEmail(customer.getEmail());
-            c.setName(customer.getEmail());
-        } else {
-            throw new IllegalStateException("customer with this id was not found.");
-        }
-    }
-
-    private Customer getById(int id){
-        Customer c = customers.stream().filter(x -> x.getId() == id).findAny().orElse(null);
-        return c;
-    }
-
-    public void delete(int id) {
-        Customer c = getById(id);
-        if(c != null){
-            customers.remove(c);
-        } else {
-            throw new IllegalStateException("customer with this id was not found.");
-        }
-    }
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
